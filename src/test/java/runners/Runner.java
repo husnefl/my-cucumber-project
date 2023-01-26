a03d9e8941d1f2ebd4a966c1d17c51e05408484b
husnefl <husneakincioglu@gmail.com>
2023-01-25 21:32:56 -0500

    wednesday

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -16,7 +16,7 @@ import org.junit.runner.RunWith;
         features = "./src/test/resources/features",
         glue = {"stepdefinitions", "hooks"},
         dryRun =false,
-        tags = "@data_tables"
+        tags = "@MedunnaRoom"
 
 )
 public class Runner {
