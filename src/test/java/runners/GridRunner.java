e4594da89843cd6f4113625e3cdf3d0ebb4c4256
husnefl <husneakincioglu@gmail.com>
2023-01-30 21:56:52 -0500

    monday

---
+++ src/test/java/runners/GridRunner.java
@@ -0,0 +1,23 @@
+package runners;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {
+                "pretty",
+                "html:target/default-cucumber-reports.html",
+                "json:target/json-reports/cucumber.json",
+                "junit:target/xml-report/cucumber.xml",
+                "rerun:target/failedRerun.txt"
+        },
+        monochrome=true,
+        features = "./src/test/resources/features",
+        glue = {"stepdefinitions"},
+        dryRun = false,
+        tags = "@grid_feature"
+//bu benim URL'im http://192.168.1.156:4444
+)
+public class GridRunner {
+}
